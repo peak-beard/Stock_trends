4d053524b15705ad32a1832c4f67655aa0636661
peak-beard <2330358@learn71.ca>
2019-09-30 05:38:51 +0200

    added the console

--- trends.py
+++ trends.py
@@ -1,12 +1,20 @@
-
+import numpy as np
 import datetime as dt
 import matplotlib.pyplot as plt
 from matplotlib import style
 import pandas as pd
 import pandas_datareader.data as web
+import datedelta as dd
+import time
 
 
 style.use('ggplot')
+df = pd.read_csv('tesla.csv')
+y_true = df['Adj Close']
+x_true = df['Date']
+m = len(x_true)
+
+
 
 def console_input() :
 
@@ -18,27 +26,33 @@ def console_input() :
         update()
         print('Sucessfully updated tesla.csv')
         console_input()
+    elif i == 'plot' :
+        plot()
 
     else:
         print('Command not found.')
         console_input()
 
-
-
 def update() :
     stop = dt.date.today()
-    start = stop - dt.timedelta(days=365)
+    start = stop - dd.datedelta(days=365)
+    print(stop)
     print(start)
     df = web.DataReader('TSLA', 'yahoo', start, stop)
     df.to_csv('tesla.csv')
+    print(start + dd.datedelta(days=365))
+
+def plot() :
+    df['Adj Close'].plot()
+    plt.show()
+    time.sleep(5)
+    console_input()
 
 
-df = pd.read_csv('tesla.csv', parse_dates=True)
-#df['Adj Close'].plot()
-#plt.show()
-axis_y = df['Adj Close']
-axis_x = df['Date']
-#print(axis_x)
-m = len(axis_y)
+def h(x, d0, d1) :
+    y_pred = x * d1
 
-print(m)
+    return y_pred
+
+#print(h(x_true[1], 0, 1))
+console_input()
